a120382192cfa95b8bb404c2388690f5f3150f3e
FilatovAV <a777f@mail.ru>
2019-05-25 12:19:42 +0300

    Add project files.

---
+++ TaskQueue/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TaskQueue.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
