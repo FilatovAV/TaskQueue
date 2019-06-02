2f46a1de84d63cdc1730299c026319b5227eb4dd
FilatovAV <a777f@mail.ru>
2019-06-02 19:54:17 +0300

    Date-time display correction

--- TaskQueue/Data/TaskQueueContextInitializer.cs
+++ TaskQueue/Data/TaskQueueContextInitializer.cs
@@ -44,13 +44,15 @@ namespace TaskQueue
                 transaction.Commit();
             }
             //Создаем задачи
+            //Уберем секунды из текущей даты
+            DateTime dateTimeNow = Convert.ToDateTime(DateTime.Now.ToString("g"));
             using (var transaction = _db.Database.BeginTransaction())
             {
                 List<Issue> _Issues = new List<Issue> {
-                    new Issue{ Id = 1, Header = "First task", Content = "Contents of the first task", CreationDate = DateTime.Now, ExecutionDate = DateTime.Now.Date.AddHours(DateTime.Now.Hour).AddMinutes(DateTime.Now.Minute + 15), StatusId = 1 },
-                    new Issue{ Id = 2, Header = "Second task", Content = "Content of the second task", CreationDate = DateTime.Now, ExecutionDate = DateTime.Now.Date.AddHours(DateTime.Now.Hour + 3).AddMinutes(DateTime.Now.Minute), StatusId = 1 },
-                    new Issue{ Id = 3, Header = "Third task", Content = "Content of the third task", CreationDate = DateTime.Now, ExecutionDate = DateTime.Now.Date.AddHours(DateTime.Now.Hour + 2).AddMinutes(DateTime.Now.Minute), StatusId = 2 },
-                    new Issue{ Id = 4, Header = "Fourth task", Content = "Content of the fourth task", CreationDate = DateTime.Now, ExecutionDate = DateTime.Now.Date.AddHours(DateTime.Now.Hour).AddMinutes(DateTime.Now.Minute + 5), StatusId = 3 }
+                    new Issue{ Id = 1, Header = "First task", Content = "Contents of the first task", CreationDate = dateTimeNow, ExecutionDate = dateTimeNow.Date + new TimeSpan(dateTimeNow.Hour + 1, dateTimeNow.Minute, 0), StatusId = 1 },
+                    new Issue{ Id = 2, Header = "Second task", Content = "Content of the second task", CreationDate = dateTimeNow, ExecutionDate = dateTimeNow.Date + new TimeSpan(dateTimeNow.Hour + 2, dateTimeNow.Minute + 50, 0), StatusId = 1 },
+                    new Issue{ Id = 3, Header = "Third task", Content = "Content of the third task", CreationDate = dateTimeNow, ExecutionDate = dateTimeNow.Date + new TimeSpan(dateTimeNow.Hour + 3, dateTimeNow.Minute, 0), StatusId = 2 },
+                    new Issue{ Id = 4, Header = "Fourth task", Content = "Content of the fourth task", CreationDate = dateTimeNow, ExecutionDate = dateTimeNow.Date + new TimeSpan(dateTimeNow.Hour + 3, dateTimeNow.Minute + 20, 0), StatusId = 3 }
                 };
 
                 await _db.Issues.AddRangeAsync(_Issues);
--- TaskQueue/ViewModels/IssueViewModel.cs
+++ TaskQueue/ViewModels/IssueViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 using TaskQueue.Domain;
@@ -11,7 +12,9 @@ namespace TaskQueue.ViewModels
         public int Id { get; set; }
         public string Header { get; set; }
         public string Content { get; set; }
+        [Display(Name = "Creation date"), DataType(DataType.DateTime)]
         public DateTime CreationDate { get; set; }
+        [Display(Name = "Execution date"), DataType(DataType.DateTime)]
         public DateTime? ExecutionDate { get; set; }
         public int? StatusId { get; set; }
         public virtual Status Status { get; set; }
